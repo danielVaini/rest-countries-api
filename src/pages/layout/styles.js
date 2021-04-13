5c3c8cb9e45c30be23da3ce70d16c6c5184d9dfc
Daniel Vaini <dnlvaini@gmail.com>
2021-04-13 17:15:00 -0300

    feat: Creating styles for layout

---
+++ src/pages/layout/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+let windowHeight = window.innerHeight;
+let windowWidht = window.innerWidth;
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+
+ 
+  min-height: ${windowHeight};
+  
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
