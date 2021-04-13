9c602c77ccb3e1894d3d64923b9b4684cb7a2b62
Daniel Vaini <dnlvaini@gmail.com>
2021-04-13 17:13:44 -0300

    feat: Creating layout

---
+++ src/pages/layout/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Layout() {
+  return (
+    <>
+      <Container>
+        <h1>Olá</h1>
+      </Container>
+    </>
+  );
+}
+
+export default Layout;
