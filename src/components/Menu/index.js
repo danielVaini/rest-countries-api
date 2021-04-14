21d2bc9f8626902e07362bfa229bca957058fdae
Daniel Vaini <dnlvaini@gmail.com>
2021-04-14 08:19:16 -0300

    feat: Creating menu bar

---
+++ src/components/Menu/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Navbar, NavContainer, H1, Div, MoonIcon, Text } from './styles';
+
+function Menu() {
+  return (
+    <>
+      <Navbar>
+        <NavContainer>
+          <H1>Where in the world?</H1>
+          <Div>
+            <MoonIcon />
+            <Text>
+              Dark mode
+            </Text>
+          </Div>
+        </NavContainer>
+      </Navbar>
+    </>
+  );
+}
+
+export default Menu;
