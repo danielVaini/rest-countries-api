8b412d406cf6a8f02463f45f2373cf972160d317
Daniel Vaini <dnlvaini@gmail.com>
2021-04-16 23:36:45 -0300

    style: Creating styles for new page

---
+++ src/pages/Flag/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ShowCard = styled.div`
+
+`;
+export const Img = styled.img`
+
+`;
+export const Details = styled.div`
+
+`;
+export const Text = styled.p`
+
+`;
