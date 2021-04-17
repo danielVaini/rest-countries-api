ef9991a175de4f4b3a983fcb75089ed1c616601c
Daniel Vaini <dnlvaini@gmail.com>
2021-04-16 23:36:17 -0300

    style: Creating styles for cards with countries

--- src/components/Cards/styles.js
+++ src/components/Cards/styles.js
@@ -18,24 +18,42 @@ export const Grid = styled.div`
 `;
 
 
-export const Card = styled.div`
+export const Card = styled.a`
+  
   width: 250px;
   height: 350px;
 
   display: flex;
   
   flex-direction: column;
-
+  
   background-color: var(--dark-blue);
   border-top-left-radius: 16px; 
   border-top-right-radius: 16px; 
 
   > img {
-  
-    min-width: 250px;
+    width: 100%;
+    height: 170px;
+    
     
     border-top-left-radius: 16px; 
     border-top-right-radius: 16px; 
+  }
+
+  > div {
+    height: 100%;
+
+    color: white;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    padding: 16px;
+
+    > p {
+      margin-bottom: 8px;
+    }
   }
 
 `;
