a5557d773380c61aa4a129424aecf6e2df0e04fd
Daniel Vaini <dnlvaini@gmail.com>
2021-04-18 08:34:16 -0300

    feat: Adding basic styles in country flag page for test

--- src/pages/Flag/styles.js
+++ src/pages/Flag/styles.js
@@ -1,7 +1,17 @@
 import styled from 'styled-components';
 
-export const ShowCard = styled.div`
+export const Container = styled.div`
+ width: 100%;
+ height: 100%;
+`;
 
+export const ShowCard = styled.div`
+  width: 100%;
+  height: 700px;
+
+  border: 1px solid white;
+
+  display: flex;
 `;
 export const Img = styled.img`
 
@@ -10,5 +20,5 @@ export const Details = styled.div`
 
 `;
 export const Text = styled.p`
-
+  color: white;
 `;
