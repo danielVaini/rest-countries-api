2c5b5585059c772a3381fc2334ab012a44d1b49f
Daniel Vaini <dnlvaini@gmail.com>
2021-04-19 21:17:06 -0300

    feat: Altering code for get select value

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,39 +1,58 @@
 import React, { useEffect, useState } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import { Grid, Card } from './styles';
+import { Grid, CardContainer, Card, InputContainer, SearchIcon } from './styles';
 import api from '../../server/api';
 
 function Cards() {
 
-const [countries, setCountries] = useState([])
+  const [countries, setCountries] = useState([])
+  const [search, setSeacrch] = useState('all')
+  useEffect(() => {
+    api.get(`/${search}`).then((res) => {
+      setCountries(res.data);
+    })
+  }, [])
 
-useEffect(() => {
-  api.get('/all').then((res) => {
-    setCountries(res.data);
-    console.log(res.data)
-  })
-}, [])
-
-  return ( 
+  return (
+    <CardContainer>
+        <InputContainer>
+          <div className="search">
+            <SearchIcon />
+            <input type="text"  placeholder="Search For a country..."/>
+          </div>
+          <div className="select-regions">
+            <select id="regions">
+              <option value="all" selected>All</option>
+              <option value="Africa">Africa</option>
+              <option value="America">Ameria</option>
+              <option value="Asia">Asia</option>
+              <option value="Europe">Europe</option>
+              <option value="Oceania">Oceania</option>
+            </select>
+          </div>
+       </InputContainer>
       <Grid>
-        {countries.map(item => {
+        {countries.map((item) => {
           return (
-            <Link key={item.alpha2Code} to={`/flag/${item.alpha2Code}`} >
+
+            <Link to={`/flag/${item.alpha2Code}`}>
             <Card>
               <img src={item.flag} alt="Flag"/>
-              <div>
-                <h3>{item.name}</h3>
-                <br/>
-                <p>Population: {item.population}</p>
-                <p>Region: {item.region}</p>
-                <p>Capital: {item.capital}</p>
+              <div className="description">
+                <h2>{item.name}</h2>
+                <div className="info">
+                  <p><strong>Population: </strong> {item.population}</p>
+                  <p><strong>Region: </strong> {item.region}</p>
+                  <p><strong>Capital: </strong>{item.capital}</p>
+                </div>
               </div>
             </Card>
-            </Link>
-          )
+          </Link>
+            )
         })}
-      </Grid>
+        </Grid>
+    </CardContainer>
   );
 }
 
