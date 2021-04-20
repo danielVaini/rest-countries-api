7802a5a654fc285fdc798568357d19ff965936f3
Daniel Vaini <dnlvaini@gmail.com>
2021-04-19 21:19:24 -0300

    feat: Adding new component for layout

--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 import Navbar from '../../components/Menu/index';
-import Search from '../../components/Search/index';
+
 import Cards from '../../components/Cards/index';
-import { Container } from './styles';
+import { Container, LayoutPage } from './styles';
 
 function Layout() {
   return (
-    <>
       <Container>
-        <Navbar />
-        <Search />
-        <Cards />
+          <Navbar />
+        <LayoutPage>
+        
+          <Cards />
+        </LayoutPage>
       </Container>
-    </>
   );
 }
 
