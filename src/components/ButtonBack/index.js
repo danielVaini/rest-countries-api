a947ed39781192e5865f12cfe30a514b3318e78c
Daniel Vaini <dnlvaini@gmail.com>
2021-04-20 23:05:46 -0300

    feat: Creating a new commponent

---
+++ src/components/ButtonBack/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Button, Arrow, Text } from './styles';
+
+function ButtonBack() {
+  return (
+    <Button>
+      <Arrow />
+      <Text>Back</Text>
+    </Button>
+  );
+}
+
+export default ButtonBack;
