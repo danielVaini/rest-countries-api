a64dbc645fc6b429e3a1155626b651a3e1c5c4bc
Daniel Vaini <dnlvaini@gmail.com>
2021-04-20 23:09:26 -0300

    feat: adding component ButtonBack

--- src/pages/Flag/index.js
+++ src/pages/Flag/index.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react';
 
 import Menu from '../../components/Menu/index';
-import { Container, ShowCard, Img, Details, Text} from './styles';
+import { Container, ShowCard, Details, Text} from './styles';
 
 import api from '../../server/api';
 import { useParams } from 'react-router-dom';
+import ButtonBack from '../../components/ButtonBack';
 
 
 function Flag() {
 
   const { code } = useParams();
   const [country, setCountry] = useState([])
+
+ 
 
   useEffect(() =>{
     api.get(`/alpha/${code}`).then((res) =>{
@@ -23,11 +26,12 @@ function Flag() {
     <Container>
       <Menu />
       <ShowCard>
-        <Img src={country.flag}/>
+      <ButtonBack />
+        <div className="flag">          
+          <img src={country.flag}/>
+        </div>
         <Details>
-          <Text>Name: {country.name}</Text>
-          <Text>Region: {country.region}</Text>
-          <Text>Population: {country.population}</Text>
+          
         </Details>
       </ShowCard>
     </Container>
