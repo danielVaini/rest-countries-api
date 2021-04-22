332f8e52a875e4d1f61a77f10f4592fa65ec471f
Daniel Vaini <dnlvaini@gmail.com>
2021-04-22 19:50:43 -0300

    feat: Adding currencie and languages for country

--- src/pages/Flag/index.js
+++ src/pages/Flag/index.js
@@ -13,11 +13,22 @@ function Flag() {
   const { code } = useParams();
   const [country, setCountry] = useState([])
 
+  const [currencie, setCurrencie] = useState('')
+  const [languages, setLanguages] = useState([])
  
 
   useEffect(() =>{
     api.get(`/alpha/${code}`).then((res) =>{
      setCountry(res.data);
+     console.log(res.data)
+     setCurrencie(res.data.currencies.map(item => item.name))
+     setLanguages(res.data.languages.map((item, index) => {
+     if(index > 0){
+       return ", " + item.name
+     }else {
+       return item.name
+     }
+     }))
     })
   }, [])
 
@@ -26,13 +37,26 @@ function Flag() {
     <Container>
       <Menu />
       <ShowCard>
+      <div >
       <ButtonBack />
+
         <div className="flag">          
           <img src={country.flag}/>
-        </div>
         <Details>
-          
+          <h1>{country.name}</h1>
+          <div>
+            <p><strong>Native Name: </strong>{country.nativeName}</p>
+            <p><strong>Population: </strong>{country.population}</p>
+            <p><strong>Region: </strong>{country.region}</p>
+            <p><strong>Sub Region: </strong>{country.subregion}</p>
+            <p><strong>Capital: </strong>{country.capital}</p>
+          </div>
+          <p><strong>Top Level Domain: </strong>{country.topLevelDomain}</p>
+          <p><strong>Currencies: </strong>{currencie}</p>
+          <p><strong>Languages: </strong>{languages}</p>
         </Details>
+        </div>
+      </div>
       </ShowCard>
     </Container>
    
