2f72322325357e50c3dd0770bdcf9540138abd71
Daniel Vaini <dnlvaini@gmail.com>
2021-04-23 22:19:47 -0300

    feat: Editing the button

--- src/components/ButtonBack/index.js
+++ src/components/ButtonBack/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import { Button, Arrow, Text } from './styles';
 
+import { Link } from 'react-router-dom';
+
 function ButtonBack() {
   return (
-    <Button>
-      <Arrow />
-      <Text>Back</Text>
+    <Button> 
+      <Link to="/">
+        <Arrow />
+        <Text>Back</Text>
+      </Link>
     </Button>
   );
 }
