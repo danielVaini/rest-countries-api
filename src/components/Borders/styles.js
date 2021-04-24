25e5326036981cc8effce7aceef266d1b2219f0d
Daniel Vaini <dnlvaini@gmail.com>
2021-04-23 22:57:16 -0300

    style: Adding style for component Borders

---
+++ src/components/Borders/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const BorderDiv = styled.div`
+  width: 60px;
+
+  border: 1px solid white;
+  margin-left: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
