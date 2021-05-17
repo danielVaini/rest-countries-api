ad0e9f89414f6ce717bc61ba25d625cd60a10809
Daniel Vaini <dnlvaini@gmail.com>
2021-05-16 23:31:24 -0300

    feat: Creating menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,21 +2,20 @@ import React from 'react';
 
 import { Navbar, NavContainer, H1, Div, MoonIcon, Text } from './styles';
 
+
 function Menu() {
   return (
-    <>
       <Navbar>
         <NavContainer>
           <H1>Where in the world?</H1>
           <Div>
             <MoonIcon />
-            <Text>
+            <Text className="btn-night">
               Dark mode
             </Text>
           </Div>
         </NavContainer>
       </Navbar>
-    </>
   );
 }
 
