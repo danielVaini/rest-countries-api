313a2e93793e6adb90c61513b9a96aeea115e7ae
Daniel Vaini <dnlvaini@gmail.com>
2021-05-16 23:30:20 -0300

    slite: Creating style for borders of country

--- src/components/Borders/styles.js
+++ src/components/Borders/styles.js
@@ -1,12 +1,18 @@
 import styled from 'styled-components';
 
 export const BorderDiv = styled.div`
-  width: 60px;
-
-  border: 1px solid white;
+  width: 65px;
+  
+  border: 1px solid var(--vr-dark-blue-LG);
   margin-left: 8px;
-
+  background-color: var(--dark-blue);
   display: flex;
   align-items: center;
   justify-content: center;
+
+  > p {
+    font-size: 12px;
+    letter-spacing: 3px;
+    font-weight: 600;
+  }
 `;
