65fbc930ab6075dd5be68d23158a86472befe3a4
Daniel Vaini <dnlvaini@gmail.com>
2021-05-23 23:38:00 -0300

    style: Altering color variable

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import { Navbar, NavContainer, H1, Div } from './styles';
 
-import { Navbar, NavContainer, H1, Div, MoonIcon, Text } from './styles';
 
 
 function Menu() {
+ 
+  const root = document.querySelector('#root');
+
+  const classAdd = () => {
+    if(root.classList == "light-mode") {
+      root.classList.remove("light-mode");
+    }else{
+      root.classList.add("light-mode");
+    }
+
+  }
+
+
+
   return (
       <Navbar>
         <NavContainer>
           <H1>Where in the world?</H1>
           <Div>
-            <MoonIcon />
-            <Text className="btn-night">
-              Dark mode
-            </Text>
+            <button className="dark-mod" onClick={() => classAdd()}>
+              Dark Mode
+            </button>
           </Div>
         </NavContainer>
       </Navbar>
