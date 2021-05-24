74cd7ae0b4ad67bd7837a330cf124db78050f6cc
Daniel Vaini <dnlvaini@gmail.com>
2021-05-23 23:30:01 -0300

    style: Altering color variable

--- src/components/Borders/styles.js
+++ src/components/Borders/styles.js
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 export const BorderDiv = styled.div`
   width: 65px;
   
-  border: 1px solid var(--vr-dark-blue-LG);
+  border: 1px solid var(--bg-dark);
   margin-left: 8px;
-  background-color: var(--dark-blue);
+  background-color: var(--card-dark);
   display: flex;
   align-items: center;
   justify-content: center;
