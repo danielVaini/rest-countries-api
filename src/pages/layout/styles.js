e0232c0b467dafe14c65989606704b2a51141dc4
Daniel Vaini <dnlvaini@gmail.com>
2021-05-23 23:42:26 -0300

    style: Altering color variable

--- src/pages/layout/styles.js
+++ src/pages/layout/styles.js
@@ -3,13 +3,15 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  height: 100%;
+  height: auto;
 
   
   display: flex;
   flex-direction: column;
   align-items: center;
  
+background-color: var(--bg-dark);
+
 `;
 
 export const LayoutPage = styled.div`
