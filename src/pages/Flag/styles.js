a2c8cba86542d5e81210b15750a0c520f046ea1a
Daniel Vaini <dnlvaini@gmail.com>
2021-05-25 22:04:19 -0300

    style: Making page in responsive

--- src/pages/Flag/styles.js
+++ src/pages/Flag/styles.js
@@ -1,16 +1,23 @@
 import styled from 'styled-components';
 
+
+
 export const Container = styled.div`
  width: 100%;
  height: 100vh;
 
-background-color: var(--bg-dark);
- 
+ background-color: var(--bg-dark);
+ @media (max-width: 1100px){
+   height: 800px;
+ }
+ @media (max-width: 680px){
+   height: 1200px;
+ }
 `;
 
 export const Layout = styled.div`
   width: 100%;
-
+  height: auto;
 
   display: flex;
   align-items: center;
@@ -58,6 +65,58 @@ export const Layout = styled.div`
 
       > p:first-child {
         margin-right: 10px;
+      }
+
+      > .borders-container {
+        display: flex;
+      }
+    }
+  }
+
+  @media (max-width: 1100px){
+    flex-direction: column;
+    .img-container{
+      width: 300px;
+      height: 190px;
+
+      margin-bottom: 80px;
+    }
+
+    .details {
+      align-items: center;
+
+    }
+  }
+
+  @media (max-width: 640px){
+
+    .img-container {
+      margin-bottom: 216px;
+    }
+    .details {
+      align-items: center;
+      .details-container {
+        flex-direction: column;
+
+        width: 100%;
+        align-items: left;
+
+        margin-bottom: 8px;
+
+        
+      }
+
+      > .borders {
+        flex-direction: column;
+        > .borders-container {
+        
+          margin-top: 16px;
+          flex-direction: column;
+          justify-content: space-around;
+          
+
+          height: 200px;
+        }
       }
     }
   }
